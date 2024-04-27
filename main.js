659e99dfa029bbe520d098c22e46cb72118c5acf
th4rau <168271929+th4rau@users.noreply.github.com>
2024-04-27 13:28:26 +0300

    source

---
+++ main.js
@@ -0,0 +1,29 @@
+const {app, BrowserWindow,Notification} = require('electron')
+const path = require('path')
+let bgw;
+
+function ihatehowelectronworks() {
+    tempw = new BrowserWindow({
+        autoHideMenuBar: true,
+        title: "bgo or something",
+        icon: path.join(__dirname, 'assets/favicon.ico'),
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            devTools: !app.isPackaged,
+        }
+    });
+
+    tempw.loadURL('https://www.boardgame-online.com')
+        .then(() => {tempw.maximize(), tempw.show(); })
+    return tempw
+}
+
+app.whenReady().then(() => {
+    bgw = ihatehowelectronworks(); 
+    bgw.webContents.setWindowOpenHandler((details) => {
+        bgw.loadURL(details.url) //just make so joining games or making games doesn't make a new window
+        
+        return { action: 'deny' }
+      });
+})
